7a65d742c080b1f9e0e06e26b4da37483e2ffe90
Shadi <shadi.0.lor@gmail.com>
2024-10-28 23:39:04 +0300

    رسالة توضيحية للتغييرات

--- script.js
+++ script.js
@@ -13,3 +13,32 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+/* الوضع الليلي */
+const themeToggleBtn = document.getElementById('theme-toggle');
+const body = document.body;
+
+// تحقق من الوضع المخزن في التخزين المحلي عند تحميل الصفحة
+if (localStorage.getItem('theme') === 'dark') {
+    body.classList.add('dark-mode');
+    themeToggleBtn.querySelector('i').classList.add('fa-sun');
+} else {
+    themeToggleBtn.querySelector('i').classList.add('fa-moon');
+}
+
+// تبديل الوضع عند النقر على زر التبديل وتخزين الإعداد الجديد
+themeToggleBtn.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    const icon = themeToggleBtn.querySelector('i');
+    
+    // تبديل أيقونة الوضع
+    icon.classList.toggle('fa-moon');
+    icon.classList.toggle('fa-sun');
+
+    // تحديث الوضع المخزن في التخزين المحلي بناءً على الوضع الحالي
+    if (body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+});
